b79227c0d5b4c18aa4ac9522b5b837ead1e5d90a
Raditya Khansa Adifa <100019946+RadityaKhansaAdifa@users.noreply.github.com>
2022-04-27 19:30:57 +0700

    Add files via upload

---
+++ praktikum6/biodata.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>BIOADATA SEDERHANA</title>
+</head>
+<body>
+<?php
+echo"DATA DIRI
+<br>
+<br>
+<table width='700' height='200' border='0' >
+<tr>
+<td width='50'>&nbsp;</td>
+<td width='200' height='5'>NAMA LENGKAP</td>
+<td>:</td>
+<td width='500'> Raditya Khansa Adifa </td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td>NAMA PANGGILAN</td>
+<td>:</td>
+<td>Radit</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td> TTL</td>
+<td> :</td>
+<td>Surabaya, 14 September 2002</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td> JURUSAN</td>
+<td>:</td>
+<td>Sistem Informasi </td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td> ALAMAT</td>
+<td>:</td>
+<td>Jl. Wiguna Selatan III No. 17 Gununganyar, Surabaya</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td> Tinggi/Berat</td>
+<td>:</td>
+<td>180 cm/90 kg</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td> STATUS</td>
+<td>:</td>
+<td>Mahasiswa</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td> AGAMA</td>
+<td>:</td>
+<td>Islam</td>
+</tr>
+<tr>
+<td height='21'>&nbsp;</td>
+<td> HOBI</td>
+<td>:</td>
+<td>Otomotif</td>
+</tr>
+</table>";
+?>   
+</body>
+</html>
